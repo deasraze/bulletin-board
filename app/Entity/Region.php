3f03ee97829a183d799e6ed03ca23ea9ed315542
theifel <condecrom@gmail.com>
2021-04-02 14:38:45 +0300

    Added region entity

---
+++ app/Entity/Region.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int|null $parent_id
+ *
+ * @property Region $parent
+ * @property Region[] $children
+*/
+class Region extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'parent_id',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(static::class, 'parent_id', 'id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(static::class, 'parent_id', 'id');
+    }
+}
