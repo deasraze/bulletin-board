2d72146e35019e3d9faaf6e3fb3ce68c6b308dc1
theifel <condecrom@gmail.com>
2021-04-02 12:23:07 +0300

    Added guard for admin panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,9 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                @can('admin-panel')
                                 <a class="dropdown-item" href="{{ route('admin.home') }}">Admin</a>
+                                @endcan
                                 <a class="dropdown-item" href="{{ route('cabinet') }}">Cabinet</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
