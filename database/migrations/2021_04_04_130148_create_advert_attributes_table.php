a61e828c921a8c1d6e4ca2ace80db28c47a79eda
theifel <condecrom@gmail.com>
2021-04-04 18:30:54 +0300

    Added migration for create advert attributes table

---
+++ database/migrations/2021_04_04_130148_create_advert_attributes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvertAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advert_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('advert_categories')->onDelete('CASCADE');
+            $table->string('name');
+            $table->string('type');
+            $table->boolean('required');
+            $table->json('variants');
+            $table->integer('sort');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advert_attributes');
+    }
+}
