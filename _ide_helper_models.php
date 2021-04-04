5c82020436f2407501363e657982b2f93d5f8134
theifel <condecrom@gmail.com>
2021-04-04 09:59:46 +0300

    Added category entity

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -10,6 +10,75 @@
  */
 
 
+namespace App\Entity\Adverts{
+/**
+ * App\Entity\Adverts\Category
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int|null $parent_id
+ * @property int $depth
+ * @property Category $parent
+ * @property Category[] $children
+ * @property int $_lft
+ * @property int $_rgt
+ * @property-read int|null $children_count
+ * @method static \Kalnoy\Nestedset\Collection|static[] all($columns = ['*'])
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category ancestorsAndSelf($id, array $columns = [])
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category ancestorsOf($id, array $columns = [])
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category applyNestedSetScope(?string $table = null)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category countErrors()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category d()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category defaultOrder(string $dir = 'asc')
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category descendantsAndSelf($id, array $columns = [])
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category descendantsOf($id, array $columns = [], $andSelf = false)
+ * @method static \Database\Factories\Entity\Adverts\CategoryFactory factory(...$parameters)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category fixSubtree($root)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category fixTree($root = null)
+ * @method static \Kalnoy\Nestedset\Collection|static[] get($columns = ['*'])
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category getNodeData($id, $required = false)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category getPlainNodeData($id, $required = false)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category getTotalErrors()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category hasChildren()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category hasParent()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category isBroken()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category leaves(array $columns = [])
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category makeGap(int $cut, int $height)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category moveNode($key, $position)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category newModelQuery()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category newQuery()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category orWhereAncestorOf(bool $id, bool $andSelf = false)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category orWhereDescendantOf($id)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category orWhereNodeBetween($values)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category orWhereNotDescendantOf($id)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category query()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category rebuildSubtree($root, array $data, $delete = false)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category rebuildTree(array $data, $delete = false, $root = null)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category reversed()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category root(array $columns = [])
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereAncestorOf($id, $andSelf = false, $boolean = 'and')
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereAncestorOrSelf($id)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereDescendantOf($id, $boolean = 'and', $not = false, $andSelf = false)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereDescendantOrSelf(string $id, string $boolean = 'and', string $not = false)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereId($value)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereIsAfter($id, $boolean = 'and')
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereIsBefore($id, $boolean = 'and')
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereIsLeaf()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereIsRoot()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereLft($value)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereName($value)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereNodeBetween($values, $boolean = 'and', $not = false)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereNotDescendantOf($id)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereParentId($value)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereRgt($value)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category whereSlug($value)
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category withDepth(string $as = 'depth')
+ * @method static \Kalnoy\Nestedset\QueryBuilder|Category withoutRoot()
+ */
+	class Category extends \Eloquent {}
+}
+
 namespace App\Entity{
 /**
  * App\Entity\Region
---
+++ app/Entity/Adverts/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity\Adverts;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int|null $parent_id
+ *
+ * @property int $depth
+ * @property Category $parent
+ * @property Category[] $children
+ */
+class Category extends Model
+{
+    use NodeTrait, HasFactory;
+
+    protected $table = 'advert_categories';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'parent_id'
+    ];
+}
