5280e7c91d7e73b602847be9b0427d250750d291
theifel <condecrom@gmail.com>
2021-04-05 14:10:14 +0300

    Added migration for user phone

--- database/factories/Entity/UserFactory.php
+++ database/factories/Entity/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories\Entity;
 
 use App\Entity\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -23,14 +24,19 @@ class UserFactory extends Factory
     public function definition()
     {
         $active = $this->faker->boolean;
+        $phoneActive = $this->faker->boolean;
 
         return [
             'name' => $this->faker->name,
             'last_name' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->unique()->phoneNumber,
+            'phone_verified' => $phoneActive,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'verify_token' =>  $active ? null : Str::uuid(),
+            'verify_token' => $active ? null : Str::uuid(),
+            'phone_verify_token' => $phoneActive ? null : Str::uuid(),
+            'phone_verify_token_expire' => $phoneActive ? null : Carbon::now()->addSeconds(300),
             'role' => $active
                 ? $this->faker->randomElement([User::ROLE_ADMIN, User::ROLE_USER])
                 : User::ROLE_USER,
