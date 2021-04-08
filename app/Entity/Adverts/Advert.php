d318c61b085a7df99f924cc3fde19e62860ac6c7
theifel <condecrom@gmail.com>
2021-04-08 14:56:00 +0300

    Added Advert entity

---
+++ app/Entity/Adverts/Advert.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity\Adverts;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property int $category_id
+ * @property int $region_id
+ * @property string $title
+ * @property string $content
+ * @property int $price
+ * @property string $address
+ * @property string $status
+ * @property string $reject_reason
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon $published_at
+ * @property Carbon $expires_at
+ */
+class Advert extends Model
+{
+    public const STATUS_DRAFT = 'draft';
+    public const STATUS_ACTIVE = 'active';
+    public const STATUS_CLOSED = 'closed';
+    public const STATUS_MODERATION = 'moderation';
+
+    protected $table = 'advert_adverts';
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'expires_at' => 'datetime',
+    ];
+
+    public static function statusesList(): array
+    {
+        return [
+            self::STATUS_DRAFT => 'Draft',
+            self::STATUS_ACTIVE => 'Active',
+            self::STATUS_CLOSED => 'Closed',
+            self::STATUS_MODERATION => 'On moderation',
+        ];
+    }
+
+    public function isDraft(): bool
+    {
+        return $this->status === self::STATUS_DRAFT;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
+
+    public function isClosed(): bool
+    {
+        return $this->status === self::STATUS_CLOSED;
+    }
+
+    public function isOnModeration(): bool
+    {
+        return $this->status === self::STATUS_MODERATION;
+    }
+}
