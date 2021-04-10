cd890093446cf280adf8315bbe5808cd10677cba
theifel <condecrom@gmail.com>
2021-04-10 12:30:09 +0300

    Added output of categories and regions on the main page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Entity\Adverts\Category;
+use App\Entity\Region;
 
 class HomeController extends Controller
 {
@@ -10,6 +12,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $regions = Region::roots()->orderBy('name')->getModels();
+        $categories = Category::whereIsRoot()->defaultOrder()->getModels();
+
+        return view('home', compact('regions', 'categories'));
     }
 }
