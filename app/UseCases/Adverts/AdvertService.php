6363d68ffc0dbebf784512fcb2fa69da6a9f0225
theifel <condecrom@gmail.com>
2021-04-10 19:19:47 +0300

    Added more methods for working with advert in AdvertService

--- app/UseCases/Adverts/AdvertService.php
+++ app/UseCases/Adverts/AdvertService.php
@@ -6,7 +6,11 @@ use App\Entity\Adverts\Advert\Advert;
 use App\Entity\Adverts\Category;
 use App\Entity\Region;
 use App\Entity\User;
+use App\Http\Requests\Adverts\AttributesRequest;
 use App\Http\Requests\Adverts\CreateRequest;
+use App\Http\Requests\Adverts\PhotoRequest;
+use App\Http\Requests\Adverts\RejectRequest;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class AdvertService
@@ -18,8 +22,6 @@ class AdvertService
         $region = $regionId ? Region::findOrFail($regionId) : null;
 
         return DB::transaction(function () use ($request, $user, $category, $region) {
-
-            /* @var Advert $advert */
             $advert = Advert::make([
                 'title' => $request['title'],
                 'content' => $request['content'],
@@ -47,5 +49,67 @@ class AdvertService
 
             return $advert;
         });
+    }
+
+    public function addPhotos(PhotoRequest $request, int $id): void
+    {
+        $advert = $this->getAdvert($id);
+
+        DB::transaction(function () use ($request, $advert) {
+            foreach ($request['files'] as $file) {
+                $advert->photos()->create([
+                    'file' => $file->store('adverts'),
+                ]);
+            }
+        });
+    }
+
+    public function sendToModeration(int $id): void
+    {
+        $advert = $this->getAdvert($id);
+        $advert->sendToModeration();
+    }
+
+    public function moderate(int $id): void
+    {
+        $advert = $this->getAdvert($id);
+        $advert->moderate(Carbon::now());
+    }
+
+    public function reject(RejectRequest $request, int $id): void
+    {
+        $advert = $this->getAdvert($id);
+        $advert->reject($request['reason']);
+    }
+
+    public function editAttributes(AttributesRequest $request, $id): void
+    {
+        $advert = $this->getAdvert($id);
+
+        DB::transaction(function () use ($advert, $request) {
+            $advert->values()->delete();
+
+            foreach ($advert->category->allAttributes() as $attribute) {
+                $value = $request['attributes'][$attribute->id] ?? null;
+
+                if ($value !== null) {
+                    $advert->values()->create([
+                        'attribute_id' => $attribute->id,
+                        'value' => $value,
+                    ]);
+                }
+            }
+        });
+    }
+
+    public function remove(int $id): void
+    {
+        $advert = $this->getAdvert($id);
+        $advert->delete();
+    }
+
+    private function getAdvert(int $id): Advert
+    {
+        return Advert::findOrFail($id);
     }
 }
