00d2701ec926a1d733c2b47f553576639e652ab2
theifel <condecrom@gmail.com>
2021-04-12 22:04:14 +0300

    Added js to display the phone number in the advert

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+$(document).on('click', '.phone-button', function () {
+    let button = $(this);
+    axios.post(button.data('source')).then(function (response) {
+        button.find('.number').html(response.data)
+    }).catch(function (error) {
+        console.error(error);
+    });
+});
