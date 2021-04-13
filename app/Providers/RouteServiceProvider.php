97acb338ae67c8d51c0b7da4d74b3833414cd5a9
theifel <condecrom@gmail.com>
2021-04-13 19:18:43 +0300

    Changed routing for adverts to adverts_path

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Router\AdvertsPath;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -35,6 +36,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Route::model('adverts_path', AdvertsPath::class);
+
         $this->configureRateLimiting();
 
         $this->routes(function () {
