dae5602c32f536abca6c22266f7fb7f3113fe340
theifel <condecrom@gmail.com>
2021-04-13 12:27:32 +0300

    Fixed AdvertController for cabinet

--- app/Http/Controllers/Cabinet/Adverts/AdvertController.php
+++ app/Http/Controllers/Cabinet/Adverts/AdvertController.php
@@ -4,20 +4,14 @@ namespace App\Http\Controllers\Cabinet\Adverts;
 
 use App\Entity\Adverts\Advert\Advert;
 use App\Http\Controllers\Controller;
-use App\Http\Middleware\FilledProfile;
 use Illuminate\Support\Facades\Auth;
 
 class AdvertController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(FilledProfile::class);
-    }
-
     public function index()
     {
-        $advert = Advert::forUser(Auth::user())->orderByDesc('id')->paginate(20);
+        $adverts = Advert::forUser(Auth::user())->orderByDesc('id')->paginate(20);
 
-        return view('cabinet.adverts.index', compact('advert'));
+        return view('cabinet.adverts.index', compact('adverts'));
     }
 }
