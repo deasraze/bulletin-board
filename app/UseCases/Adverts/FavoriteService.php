89e030acda65f4629251d8e0191bde6f9aca5e71
theifel <condecrom@gmail.com>
2021-04-16 09:52:35 +0300

    Added FavoriteService

---
+++ app/UseCases/Adverts/FavoriteService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\UseCases\Adverts;
+
+use App\Entity\Adverts\Advert\Advert;
+use App\Entity\User;
+
+class FavoriteService
+{
+    public function add(int $userId, int $advertId): void
+    {
+        $user = $this->getUser($userId);
+        $advert = $this->getAdvert($advertId);
+
+        $user->addToFavorites($advert->id);
+    }
+
+    public function remove(int $userId, int $advertId): void
+    {
+        $user = $this->getUser($userId);
+        $advert = $this->getAdvert($advertId);
+
+        $user->removeFromFavorites($advert->id);
+    }
+
+    private function getUser(int $userId): User
+    {
+        return User::findOrFail($userId);
+    }
+
+    private function getAdvert(int $advertId): Advert
+    {
+        return Advert::findOrFail($advertId);
+    }
+}
