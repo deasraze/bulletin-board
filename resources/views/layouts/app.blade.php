78eec3f8b4e564fce90af7bb9a92b0c2683ee8c6
theifel <condecrom@gmail.com>
2021-04-16 16:02:53 +0300

    Added search section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,39 @@
             </div>
         </div>
     </nav>
+
+    @section('search')
+        <div class="search-bar pt-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <form action="{{ route('adverts.index') }}" method="GET">
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="text"
+                                               value="{{ request('text') }}" placeholder="Search for...">
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="form-group">
+                                        <button class="btn btn-light border" type="submit"><span class="fa fa-search"></span> Search</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-3 text-right">
+                        <p>
+                            <a href="{{ route('cabinet.adverts.create') }}" class="btn btn-success">
+                                <span class="fa fa-plus"></span> Add New Advertisement
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @show
 </header>
 
 <main class="py-4 app-content">
