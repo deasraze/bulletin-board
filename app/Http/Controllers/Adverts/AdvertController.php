d96a941d00bb85bcdfd8d4b39001be36c24e12e4
theifel <condecrom@gmail.com>
2021-04-16 10:57:09 +0300

    Added transfer user data for view advert

--- app/Http/Controllers/Adverts/AdvertController.php
+++ app/Http/Controllers/Adverts/AdvertController.php
@@ -7,6 +7,7 @@ use App\Entity\Adverts\Category;
 use App\Entity\Region;
 use App\Http\Controllers\Controller;
 use App\Http\Router\AdvertsPath;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class AdvertController extends Controller
@@ -38,8 +39,9 @@ class AdvertController extends Controller
     public function show(Advert $advert)
     {
         $this->checkAccess($advert);
+        $user = Auth::user();
 
-        return view('adverts.show', compact('advert'));
+        return view('adverts.show', compact('advert', 'user'));
     }
 
     public function phone(Advert $advert): string
