8109b63882a365b7754a51022153096c11a54aa3
theifel <condecrom@gmail.com>
2021-04-17 19:52:15 +0300

    Added InitCommand

---
+++ app/Console/Commands/Search/InitCommand.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Console\Commands\Search;
+
+use Elasticsearch\Client;
+use Elasticsearch\Common\Exceptions\Missing404Exception;
+use Illuminate\Console\Command;
+
+class InitCommand extends Command
+{
+    protected $signature = 'search:init';
+
+    protected $description = 'Initialization adverts index for elastic search';
+
+    private Client $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+        parent::__construct();
+    }
+
+    public function handle(): int
+    {
+        try {
+            $this->client->indices()->delete([
+                'index' => 'adverts',
+            ]);
+        } catch (Missing404Exception $e) {
+        }
+
+        $this->client->indices()->create([
+            'index' => 'adverts',
+            'body' => [
+                'mappings' => [
+                    '_source' => [
+                        'enabled' => true,
+                    ],
+                    'properties' => [
+                        'id' => [
+                            'type' => 'integer',
+                        ],
+                        'published_at' => [
+                            'type' => 'date',
+                        ],
+                        'title' => [
+                            'type' => 'text',
+                        ],
+                        'content' => [
+                            'type' => 'text',
+                        ],
+                        'price' => [
+                            'type' => 'integer',
+                        ],
+                        'status' => [
+                            'type' => 'keyword',
+                        ],
+                        'categories' => [
+                            'type' => 'integer',
+                        ],
+                        'regions' => [
+                            'type' => 'integer',
+                        ],
+                        'values' => [
+                            'type' => 'nested',
+                            'properties' => [
+                                'attribute' => [
+                                    'type' => 'integer',
+                                ],
+                                'value_string' => [
+                                    'type' => 'keyword',
+                                ],
+                                'value_int' => [
+                                    'type' => 'integer',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'settings' => [
+                    'analysis' => [
+                        'char_filter' => [
+                            'replace' => [
+                                'type' => 'mapping',
+                                'mappings' => [
+                                    '&=> and '
+                                ],
+                            ],
+                        ],
+                        'filter' => [
+                            'word_delimiter' => [
+                                'type' => 'word_delimiter',
+                                'split_on_numerics' => false,
+                                'split_on_case_change' => true,
+                                'generate_word_parts' => true,
+                                'generate_number_parts' => true,
+                                'catenate_all' => true,
+                                'preserve_original' => true,
+                                'catenate_numbers' => true,
+                            ],
+                            'trigrams' => [
+                                'type' => 'ngram',
+                                'min_gram' => 4,
+                                'max_gram' => 6,
+                            ],
+                        ],
+                        'analyzer' => [
+                            'default' => [
+                                'type' => 'custom',
+                                'char_filter' => [
+                                    'html_strip',
+                                    'replace',
+                                ],
+                                'tokenizer' => 'whitespace',
+                                'filter' => [
+                                    'lowercase',
+                                    'word_delimiter',
+                                    'trigrams',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'max_ngram_diff' => 2,
+                ],
+            ],
+        ]);
+
+        $this->info('Success!');
+
+        return 0;
+    }
+}
