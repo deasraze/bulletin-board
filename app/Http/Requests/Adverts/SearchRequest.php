42a1a2350c9ddf75e3a09f4deee0c36e83425f4b
theifel <condecrom@gmail.com>
2021-04-20 10:55:37 +0300

    Added validation for price inputs

--- app/Http/Requests/Adverts/SearchRequest.php
+++ app/Http/Requests/Adverts/SearchRequest.php
@@ -15,6 +15,8 @@ class SearchRequest extends FormRequest
     {
         return [
             'text' => 'nullable|string',
+            'price_from' => 'nullable|numeric',
+            'price_to' => 'nullable|numeric',
             'attrs.*.equals' => 'nullable|string',
             'attrs.*.from' => 'nullable|numeric',
             'attrs.*.to' => 'nullable|numeric',
