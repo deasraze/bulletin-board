f3b596e1ad3d77fc5ba316d8b5fac0a16f60c3b2
theifel <condecrom@gmail.com>
2021-04-21 16:02:12 +0300

    Changed EditRequest

--- app/Http/Requests/Banner/EditRequest.php
+++ app/Http/Requests/Banner/EditRequest.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Requests\Banner;
 
-class EditRequest extends CreateRequest
-{
+use Illuminate\Foundation\Http\FormRequest;
 
+class EditRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'limit' => 'required|integer',
+            'url' => 'required|url',
+        ];
+    }
 }
