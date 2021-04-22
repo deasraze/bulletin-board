17c8b7e7ecb5ce488db2ce9506f8a8c21a92e74b
theifel <condecrom@gmail.com>
2021-04-22 15:20:38 +0300

    Added reindex banners for es

--- app/Console/Commands/Search/ReindexCommand.php
+++ app/Console/Commands/Search/ReindexCommand.php
@@ -3,33 +3,52 @@
 namespace App\Console\Commands\Search;
 
 use App\Entity\Adverts\Advert\Advert;
+use App\Entity\Banner\Banner;
 use App\Services\Search\AdvertIndexer;
+use App\Services\Search\BannerIndexer;
 use Illuminate\Console\Command;
 
 class ReindexCommand extends Command
 {
     protected $signature = 'search:reindex';
 
-    protected $description = 'Reindex all active adverts';
+    protected $description = 'Reindex all indexes for elastic search';
 
-    private AdvertIndexer $indexer;
+    private AdvertIndexer $adverts;
+    private BannerIndexer $banners;
 
-    public function __construct(AdvertIndexer $indexer)
+    public function __construct(AdvertIndexer $adverts, BannerIndexer $banners)
     {
-        $this->indexer = $indexer;
+        $this->adverts = $adverts;
+        $this->banners = $banners;
         parent::__construct();
     }
 
     public function handle(): int
     {
-        $this->indexer->clear();
-
-        foreach (Advert::active()->orderBy('id')->cursor() as $advert) {
-            $this->indexer->index($advert);
-        }
+        $this->reindexAdverts();
+        $this->reindexBanners();
 
         $this->info('Success!');
 
         return 0;
+    }
+
+    private function reindexAdverts(): void
+    {
+        $this->adverts->clear();
+
+        foreach (Advert::active()->orderBy('id')->cursor() as $advert) {
+            $this->adverts->index($advert);
+        }
+    }
+
+    private function reindexBanners(): void
+    {
+        $this->banners->clear();
+
+        foreach (Banner::active()->orderBy('id')->cursor() as $banner) {
+            $this->banners->index($banner);
+        }
     }
 }
