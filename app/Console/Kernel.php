a61d2bda660ea0014c152faea0a61e4cde200864
theifel <condecrom@gmail.com>
2021-04-23 08:33:27 +0300

    Added schedule for banner:expire

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('advert:expire')->hourly();
+        $schedule->command('banner:expire')->daily();
     }
 
     /**
