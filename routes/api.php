66ce5d652ee90f4b02c8b08e94a402d2081862c3
theifel <condecrom@gmail.com>
2021-04-24 11:33:24 +0300

    Added RegisterController for API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'home']);
+Route::post('/register', [RegisterController::class, 'register']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
