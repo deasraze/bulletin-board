4c4ff7e9f516944ddaba3cad856071c3fe4ea664
theifel <condecrom@gmail.com>
2021-04-24 10:36:53 +0300

    Added HomeController for API

---
+++ app/Http/Controllers/Api/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function home(): array
+    {
+        return [
+            'name' => 'Laravel board API',
+        ];
+    }
+}
