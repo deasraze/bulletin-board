c4b9155c7c3173f3a015a7702eed3cf26b8e1656
theifel <condecrom@gmail.com>
2021-04-25 11:44:08 +0300

    Added routes for api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\Auth\RegisterController;
-use App\Http\Controllers\Api\HomeController;
-use App\Http\Controllers\Api\User\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'home']);
-Route::post('/register', [RegisterController::class, 'register']);
+Route::group(['namespace' => 'App\\Http\\Controllers\\Api'], function () {
+    Route::get('/', 'HomeController@home');
+    Route::post('/register', 'HomeController@register');
 
-Route::middleware('auth:api')->get('/user', [ProfileController::class, 'show']);
+    Route::middleware('auth:api')->group(function () {
+        Route::resource('adverts', 'Adverts\AdvertController')->only('index', 'show');
+        Route::post('/adverts/{advert}/favorite', 'Adverts\FavoriteController@add');
+        Route::delete('/adverts/{advert}/favorite', 'Adverts\FavoriteController@remove');
+
+        Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
+            Route::get('/', 'ProfileController@show');
+            Route::put('/', 'ProfileController@update');
+            Route::get('/favorites', 'FavoriteController@index');
+            Route::delete('/favorites/{advert}', 'FavoriteController@remove');
+
+            Route::resource('adverts', 'AdvertController')->only('index', 'show', 'update', 'destroy');
+            Route::post('/adverts/create/{category}/{region?}', 'AdvertController@store');
+
+            Route::put('/adverts/{advert}/photos', 'AdvertController@photos');
+            Route::put('/adverts/{advert}/attributes', 'AdvertController@attributes');
+            Route::post('/adverts/{advert}/send', 'AdvertController@send');
+            Route::post('/adverts/{advert}/close', 'AdvertController@close');
+        });
+    });
+});
