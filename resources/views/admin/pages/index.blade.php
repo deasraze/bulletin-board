71729a793beb74f5a4a5f07d8f2b9b067f31f921
theifel <condecrom@gmail.com>
2021-05-12 08:32:37 +0300

    Added page views for admin panel

---
+++ resources/views/admin/pages/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('admin.pages._nav')
+
+    <p><a href="{{ route('admin.pages.create') }}" class="btn btn-success">Add Page</a></p>
+
+    <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+            <th>Title</th>
+            <th>Menu title</th>
+            <th>Slug</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach ($pages as $page)
+            <tr>
+                <td>
+                    @for ($i = 0; $i < $page->depth; $i++) &mdash; @endfor
+                    <a href="{{ route('admin.pages.show', $page) }}">{{ $page->title }}</a>
+                </td>
+                <td>{{ $page->menu_title }}</td>
+                <td>{{ $page->slug }}</td>
+                <td>
+                    <div class="d-flex flex-row justify-content-center">
+                        <form method="POST" action="{{ route('admin.pages.first', $page) }}" class="mr-1">
+                            @csrf
+                            <button class="btn btn-sm btn-outline-primary"><span class="fa fa-angle-double-up"></span></button>
+                        </form>
+                        <form method="POST" action="{{ route('admin.pages.up', $page) }}" class="mr-1">
+                            @csrf
+                            <button class="btn btn-sm btn-outline-primary"><span class="fa fa-angle-up"></span></button>
+                        </form>
+                        <form method="POST" action="{{ route('admin.pages.down', $page) }}" class="mr-1">
+                            @csrf
+                            <button class="btn btn-sm btn-outline-primary"><span class="fa fa-angle-down"></span></button>
+                        </form>
+                        <form method="POST" action="{{ route('admin.pages.last', $page) }}" class="mr-1">
+                            @csrf
+                            <button class="btn btn-sm btn-outline-primary"><span class="fa fa-angle-double-down"></span></button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+
+        </tbody>
+    </table>
+@endsection
