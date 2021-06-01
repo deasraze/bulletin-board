71bbc70ca4fef6fad2f6e3814532684c71484faa
theifel <condecrom@gmail.com>
2021-06-01 08:26:19 +0300

    Added ReindexAdvert job

---
+++ app/Jobs/Advert/ReindexAdvert.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Jobs\Advert;
+
+use App\Entity\Adverts\Advert\Advert;
+use App\Services\Search\AdvertIndexer;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ReindexAdvert implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private Advert $advert;
+
+    public function __construct(Advert $advert)
+    {
+        $this->advert = $advert;
+    }
+
+    public function handle(AdvertIndexer $indexer): void
+    {
+        $indexer->index($this->advert);
+    }
+}
